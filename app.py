7f51a32b6614e911950836caa5da9e56c8eebf96
sulman shaikh <sulmanmh@gmail.com>
2020-03-12 17:51:47 +0000

    added JWT with more verbs

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = '6565870'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity) # /auth endpointx
 
 items = [
 
@@ -14,6 +20,7 @@ items = [
 
 
 class Item(Resource):
+	@jwt_required()
 	def get(self, name):
 		item = next(filter(lambda n: n['name'] == name, items), None)
 		if item:
@@ -21,16 +28,40 @@ class Item(Resource):
 		return {'item': item}, 404
 
 	def post(self, name):
+		parser = self.get_payload_parser([('price', float)])
+		data = parser.parse_args()
+		msg, status = self.create_update_item(name, data, 'POST')
+		return msg, status
+
+	def put(self, name):
+		parser = self.get_payload_parser([('price', float)])
+		data = parser.parse_args()
+		msg, status = self.create_update_item(name, data, 'PUT')
+		return msg, status
+
+	def create_update_item(self, name, data, mode):
 		item = next(filter(lambda n: n['name'] == name, items), None)
 		if item:
-			return {'item': f'{name} already exists'}, 403
-		data = request.get_json() # silent=True or force=True
+			if mode == 'PUT':
+				item.update(data)
+				return {'item': f'{name} updated with price'}, 200
+			if mode == 'POST':
+				return {'item': f'{name} already exists'}, 403				
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
-		return item, 201
+		return item, 201		
 
-	def put(self):
-		pass		
+	def get_payload_parser(self, req_args: list, required=True):
+		parser = reqparse.RequestParser()
+		for arg in req_args:
+			parser.add_argument(arg[0], type=arg[1], required=True, help=f"{arg[0]} cannot be empty!")		
+		return parser
+
+	def delete(self, name):
+		global items
+		items = list(filter(lambda n: n['name'] != name, items))
+		return {'item': f'{name} item deleted'}, 200
+
 
 class ItemList(Resource):
 	def get(self):
