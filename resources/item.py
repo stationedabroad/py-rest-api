f223f28503d8402c406c2695c2d119f86c2b401e
sulman shaikh <sulmanmh@gmail.com>
2020-04-03 02:22:43 +0100

    item model typos fixed, create_update_item refactored

--- resources/item.py
+++ resources/item.py
@@ -10,27 +10,33 @@ class Item(Resource):
     
     @jwt_required()
     def get(self, name):
-        item = Item.item_by_name(name)
+        item = Item.find_by_name(name)
         if item:
             return {'item': item}, 200
         return {'message': 'itme not found'}, 404
     
     def post(self, name):
-        if Item.item_by_name(name):
+        if Item.find_by_name(name):
             return {'message': 'item with name {} already exists'.format(name)}, 400
         parser = self.get_payload_parser([('price', float)])
         data = parser.parse_args()
-        item, status = ItemModel.create_update_item(name, data, 'POST')
-        return item, status
+        new_item = ItemModel(name, data['price'])
+        item_posted = new_item.create_update_item('POST')
+        if item_posted:
+            return item_posted.as_json(), 201
+        return 500
     
     def put(self, name):
         mode, status = 'PUT', 204
-        if not ItemModel.item_by_name(name):
+        if not ItemModel.find_by_name(name):
             mode, status = 'POST', 201
         parser = self.get_payload_parser([('price', float)])
         data = parser.parse_args()
-        msg, status = ItemModel.create_update_item(name, data, mode)
-        return msg, status
+        updated_item = ItemModel(name, data['price'])
+        item_updated = updated_item.create_update_item(mode)
+        if item_updated:
+            return updated_item.as_json(), status
+        return 500
 
     def get_payload_parser(self, req_args: list, required=True):
 	    parser = reqparse.RequestParser()
@@ -39,18 +45,19 @@ class Item(Resource):
 	    return parser
 
     def delete(self, name):
-        if not ItemModel.item_by_name(name):
+        item_delete = ItemModel.find_by_name(name)
+        if not item_delete:
             return {'message': 'item with name {} does not exist'.format(name)}, 400            
         query = """
                     delete from items where name = '{}'
-                """.format(name)
+                """.format(item_delete.name)
         connection = sqlite3.connect(DB_NAME)
         cursor = connection.cursor()
         cursor.execute(query)
 
         connection.commit()
         connection.close()
-        return {'message': 'item {} successfully deleted'.format(name)}, 200
+        return {'message': 'item {} successfully deleted'.format(item_delete.name)}, 200
 
 
 class ItemList(Resource):
