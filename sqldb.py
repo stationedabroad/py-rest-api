f96d66e3de28ca89e6accbcb0f9930d2bca2c6f3
sulman shaikh <sulmanmh@gmail.com>
2020-04-17 23:36:35 +0100

    default return type added to database uri lookup

--- sqldb.py
+++ sqldb.py
@@ -2,7 +2,7 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 # Heroku env variable DATABASE_URL (prod use), otherwise local sqlite
-db_uri = os.environ.get('DATABASE_URL')
+db_uri = os.environ.get('DATABASE_URL', None)
 mode = 'prod'
 if not db_uri: 
     db_uri = 'sqlite:///appdata.db'
