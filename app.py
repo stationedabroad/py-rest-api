fb39a78de31ea9505c3d18856707dfcadc107f23
sulman shaikh <sulmanmh@gmail.com>
2020-04-17 23:30:19 +0100

    prod and dev DB access added {prod: postgres, dev: sqlite}

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from sqldb import db
+from sqldb import db, db_uri
 
 # port & debug
 # env variable: FLASK_RUN_PORT=5001
 # env variable: FLASK_DEBUG=1
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appdata.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '6565870'
 api = Api(app)
@@ -28,7 +28,7 @@ jwt = JWT(app, authenticate, identity) # /auth endpointx
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
-api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Store, '/store/<string:name>vi')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
--- sqldb.py
+++ sqldb.py
@@ -1,3 +1,7 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
+
+# Heroku env variable DATABASE_URL (prod use), otherwise local sqlite
+db_uri = os.environ.get('DATABASE_URL', 'sqlite:///appdata.db')
 
 db = SQLAlchemy()
